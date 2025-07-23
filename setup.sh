12749a3e701dcc956ce9a271a02b50929aece3e8
FaisalALFodialy <faisalalfodialy@hotmail.com>
2025-07-23 18:20:52 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Exit on any error
+set -e
+
 # Install Chromium and Chromedriver
 apt-get update
-apt-get install -y chromium-browser chromium-chromedriver
+apt-get install -y chromium chromium-driver
 
-# Link to expected locations
-ln -s /usr/bin/chromedriver /usr/local/bin/chromedriver
-ln -s /usr/bin/chromium-browser /usr/bin/chrome
+# Link chromium and chromedriver to expected locations
+ln -s /usr/bin/chromium /usr/bin/chrome || true
+ln -s /usr/lib/chromium/chromedriver /usr/bin/chromedriver || ln -s /usr/bin/chromedriver /usr/bin/chromedriver
