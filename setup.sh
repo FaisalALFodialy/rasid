ab3a5e301992ccabe25d1f0ec20cb5f931ad471c
FaisalALFodialy <faisalalfodialy@hotmail.com>
2025-07-27 00:22:38 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@
 apt-get update
 apt-get install -y chromium-browser chromium-chromedriver
 
+pip install playwright
+playwright install chromium
+
 # Create symlinks so selenium can find Chrome and Chromedriver
 ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
 ln -s /usr/lib/chromium-browser/chromedriver /usr/bin/chromedriver
