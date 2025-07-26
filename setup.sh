c72f0bf0f5e503e7f4eb2490c01b18fa28667b11
FaisalALFodialy <faisalalfodialy@hotmail.com>
2025-07-26 22:01:20 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Install Chromium and ChromeDriver
+# Install Chromium
 apt-get update
-apt-get install -y chromium chromium-driver
+apt-get install -y chromium-browser chromium-chromedriver
 
-# Symlinks (very important)
-ln -s /usr/bin/chromium /usr/bin/google-chrome
-ln -s /usr/lib/chromium/chromedriver /usr/local/bin/chromedriver
+# Create symlinks so selenium can find Chrome and Chromedriver
+ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
+ln -s /usr/lib/chromium-browser/chromedriver /usr/bin/chromedriver
