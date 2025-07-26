bfd4aae3aeaf20240f4a6376170d61541d3538a4
FaisalALFodialy <faisalalfodialy@hotmail.com>
2025-07-26 11:55:38 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-apt-get update
-apt-get install -y chromium-driver chromium
-
-# روابط للمسارات المتوقعة من Selenium
-ln -sf /usr/bin/chromedriver /usr/local/bin/chromedriver
-ln -sf /usr/bin/chromium /usr/bin/google-chrome
